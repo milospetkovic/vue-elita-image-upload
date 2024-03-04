4b3611a432894758cc29e56b7aa9cfa0715857fb
Miloš Petković <elitasoft.web@gmail.com>
2024-03-04 09:45:44 +0100

    * added left and right rotate buttons

--- src/Uploader.vue
+++ src/Uploader.vue
@@ -2,68 +2,81 @@
     <div>
         <div class="mu-container" :class="isInvalid?'mu-red-border':''">
             <Loader
-                color="#0275d8" 
-                :active="isLoading" 
-                spinner="line-scale" 
-                background-color = 'rgba(255, 255, 255, .4)'
+                    color="#0275d8"
+                    :active="isLoading"
+                    spinner="line-scale"
+                    background-color = 'rgba(255, 255, 255, .4)'
             />
             <div class="mu-elements-wraper">
-                
+
                 <!--UPLOAD BUTTON-->
                 <div class="mu-plusbox-container">
                     <label for="mu-file-input" class="mu-plusbox">
                         <svg
-                            class="mu-plus-icon"
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="1em" 
-                            height="1em" 
-                            preserveAspectRatio="xMidYMid meet" 
-                            viewBox="0 0 24 24">
-                                <g fill="none">
-                                    <path 
-                                        d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11s11-4.925 11-11S18.075 1 12 1zm1 15a1 1 0 1 1-2 0v-3H8a1 1 0 1 1 0-2h3V8a1 1 0 1 1 2 0v3h3a1 1 0 1 1 0 2h-3v3z" 
+                                class="mu-plus-icon"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                preserveAspectRatio="xMidYMid meet"
+                                viewBox="0 0 24 24">
+                            <g fill="none">
+                                <path
+                                        d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11s11-4.925 11-11S18.075 1 12 1zm1 15a1 1 0 1 1-2 0v-3H8a1 1 0 1 1 0-2h3V8a1 1 0 1 1 2 0v3h3a1 1 0 1 1 0 2h-3v3z"
                                         fill="currentColor"/>
-                                </g>
+                            </g>
                         </svg>
-                    </label>     
+                    </label>
                     <input @change="fileChange" id="mu-file-input" type="file" accept="image/*" multiple hidden>
                 </div>
 
                 <!--IMAGES PREVIEW-->
-                
                 <div v-for="(image, index) in savedMedia" :key="index" class="mu-image-container">
                     <img :src="location +'/'+ image.name" alt=""  class="mu-images-preview">
                     <button @click="removeSavedMedia(index)" class="mu-close-btn" type="button">
-                        <svg 
-                            class='mu-times-icon' 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="0.65em" 
-                            height="0.65em" 
-                            preserveAspectRatio="xMidYMid meet" 
-                            viewBox="0 0 352 512">
-                                <path 
-                                    d="m242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28L75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256L9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-                                    fill="currentColor"    
-                                />
-                        </svg>
+                        <i class="mdi mdi-delete text-xl"></i>
                     </button>
+                    <template v-if="showLeftRotateButton">
+                        <button @click="rotateLeft(index)" class="mu-left-rotate-btn" type="button">
+                            <i class="mdi mdi-arrow-u-left-top-bold text-xl"></i>
+                        </button>
+                    </template>
+                    <template v-if="showRightRotateButton">
+                        <button @click="rotateRight(index)" class="mu-right-rotate-btn" type="button">
+                            <i class="mdi mdi-arrow-u-right-top-bold text-xl"></i>
+                        </button>
+                    </template>
                 </div>
                 <div v-for="(image, index) in addedMedia" :key="index" class="mu-image-container">
-                    <img :src="image.url" alt=""  class="mu-images-preview">
-                    <button @click="removeAddedMedia(index)" class="mu-close-btn" type="button">
-                        <svg 
-                            class='mu-times-icon' 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="0.65em" 
-                            height="0.65em" 
-                            preserveAspectRatio="xMidYMid meet" 
-                            viewBox="0 0 352 512">
-                                <path 
-                                    d="m242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28L75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256L9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-                                    fill="currentColor"    
-                                />
-                        </svg>
+                    <!--                    <img :src="image.url" alt=""  class="mu-images-preview">-->
+                    <canvas :ref="'canvas' + index" class="mu-images-preview2"></canvas>
+                    <button
+                            @click="removeAddedMedia(index)"
+                            :disabled="isButtonDisabled(index)"
+                            class="mu-close-btn"
+                            type="button"
+                    >
+                        <i class="mdi mdi-delete text-xl"></i>
                     </button>
+                    <template v-if="showLeftRotateButton">
+                        <button
+                                @click="rotateLeft(index)"
+                                :disabled="isButtonDisabled(index)"
+                                class="mu-left-rotate-btn"
+                                type="button"
+                        >
+                            <i class="mdi mdi-arrow-u-left-top-bold text-xl"></i>
+                        </button>
+                    </template>
+                    <template v-if="showRightRotateButton">
+                        <button
+                                @click="rotateRight(index)"
+                                :disabled="isButtonDisabled(index)"
+                                class="mu-right-rotate-btn"
+                                type="button"
+                        >
+                            <i class="mdi mdi-arrow-u-right-top-bold text-xl"></i>
+                        </button>
+                    </template>
                 </div>
             </div>
         </div>
@@ -115,7 +128,15 @@
             warnings:{
                 type: Boolean,
                 default: true
-            }
+            },
+            showLeftRotateButton:{
+                type: Boolean,
+                default: false,
+            },
+            showRightRotateButton:{
+                type: Boolean,
+                default: false,
+            },
         },
         mounted() {
             this.init()
@@ -125,9 +146,11 @@
                 addedMedia:[],
                 savedMedia:[],
                 removedMedia:[],
-
                 isLoading:true
             }
+        },
+        watch: {
+            watchedMedia: 'renderImages',
         },
         methods:{
             init(){
@@ -138,7 +161,7 @@
                         this.savedMedia[index].url = this.location + "/" + image.name
                     }
                 });
-                
+
                 setTimeout(() => this.isLoading = false, 1000)
 
                 this.$emit('init', this.allMedia)
@@ -153,11 +176,11 @@
                             let formData = new FormData
                             let url = URL.createObjectURL(files[i])
                             formData.set('image', files[i])
-    
+
                             const {data} = await axios.post(this.server, formData)
                             let addedImage = {url:url, name:data.name, size:files[i].size, type:files[i].type}
                             this.addedMedia.push(addedImage)
-    
+
                             this.$emit('change', this.allMedia)
                             this.$emit('add', addedImage, this.addedMedia)
                         }else{
@@ -192,13 +215,100 @@
 
                 this.$emit('change', this.allMedia)
                 this.$emit('remove', removedImage, this.removedMedia)
-            }
-            
+            },
+            renderImages() {
+                console.log('renderimages ');
+                // Koristimo this.$nextTick() kako bismo osigurali da se refs objekat ažurira
+                this.$nextTick(() => {
+                    this.addedMedia.forEach((image, index) => {
+                        const canvas = this.$refs['canvas' + index][0];
+                        const ctx = canvas.getContext('2d');
+
+                        // Kreiramo novu sliku
+                        const img = new Image();
+                        img.onload = () => {
+
+                            canvas.width = 140; // Postavite željenu širinu
+                            canvas.height = 90; // Postavite željenu visinu
+
+                            // Postavljanje antialiasing-a
+                            ctx.imageSmoothingEnabled = true;
+                            ctx.imageSmoothingQuality = 'high';
+
+                            // Postavljanje dimenzija i položaja slike unutar canvasa
+                            const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+                            const scaledWidth = img.width * scale;
+                            const scaledHeight = img.height * scale;
+                            const offsetX = (canvas.width - scaledWidth) / 2;
+                            const offsetY = (canvas.height - scaledHeight) / 2;
+
+                            // Crtanje slike na canvasu
+                            ctx.drawImage(img, offsetX, offsetY, scaledWidth, scaledHeight);
+                        };
+                        img.src = image.url;
+                    });
+                });
+            },
+            rotateLeft(index) {
+                const canvas = this.$refs['canvas' + index][0];
+                const ctx = canvas.getContext('2d');
+                const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+                // Rotiranje dimenzija canvasa
+                const temp = canvas.width;
+                canvas.width = canvas.height;
+                canvas.height = temp;
+
+                // Postavljanje antialiasing-a
+                ctx.imageSmoothingEnabled = true;
+                ctx.imageSmoothingQuality = 'high';
+
+                // Centriranje slike u novim dimenzijama canvasa
+                const offsetX = (canvas.width - imgData.height) / 2;
+                const offsetY = (canvas.height - imgData.width) / 2;
+
+                // Rotiranje slike za 90 stepeni ulevo i crtanje na canvasu
+                ctx.putImageData(this.rotateImageData(imgData), offsetX, offsetY);
+            },
+            rotateRight(index) {
+                // Rotiranje slike za 90 stepeni ulevo
+                const canvas = this.$refs['canvas' + index][0];
+                const ctx = canvas.getContext('2d');
+                const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+                canvas.width = imgData.height;
+                canvas.height = imgData.width;
+                ctx.putImageData(this.rotateImageData(imgData), 0, 0);
+            },
+            rotateImageData(imgData) {
+                const newImgData = new ImageData(imgData.height, imgData.width);
+
+                for (let x = 0; x < imgData.width; x++) {
+                    for (let y = 0; y < imgData.height; y++) {
+                        const newIndex = (x * imgData.height + (imgData.height - y)) * 4;
+                        const oldIndex = (y * imgData.width + x) * 4;
+
+                        newImgData.data[newIndex] = imgData.data[oldIndex];
+                        newImgData.data[newIndex + 1] = imgData.data[oldIndex + 1];
+                        newImgData.data[newIndex + 2] = imgData.data[oldIndex + 2];
+                        newImgData.data[newIndex + 3] = imgData.data[oldIndex + 3];
+                    }
+                }
+
+                return newImgData;
+            },
+            isButtonDisabled(index) {
+                return true;
+            },
         },
         computed:{
             allMedia(){
                 return [...this.savedMedia, ...this.addedMedia];
-            }
+            },
+            // Computed svojstvo koje vraća kopiju niza addedMedia
+            watchedMedia() {
+                return JSON.parse(JSON.stringify(this.addedMedia));
+            },
         },
         emits: [
             'init',
@@ -212,119 +322,152 @@
             Loader
         }
     }
-        
 </script>
 
 <style scoped>
 
-.mu-container{
-    background-color: #fbfbfb !important;
-    border-radius: 5px !important;
-    border-style: solid !important;
-    border: 1px solid #9b9b9b !important;
-    box-sizing: border-box !important;
-    width: 100% !important;
-    height: auto !important;
-}
+    .mu-container{
+        background-color: #fbfbfb !important;
+        border-radius: 5px !important;
+        border-style: solid !important;
+        border: 1px solid #9b9b9b !important;
+        box-sizing: border-box !important;
+        width: 100% !important;
+        height: auto !important;
+    }
 
-/* ----elements-wrapper--- */
+    /* ----elements-wrapper--- */
 
-.mu-elements-wraper {
-  padding: 1rem !important;
-  display: flex !important;
-  flex-wrap: wrap !important;
-}
+    .mu-elements-wraper {
+        padding: 1rem !important;
+        display: flex !important;
+        flex-wrap: wrap !important;
+    }
 
-/* ----plusbox--- */
+    /* ----plusbox--- */
 
-.mu-plusbox-container{
-    display: inline-flex !important;
-    height: 90px !important;
-    width: 140px !important;
-    margin: 0.25rem !important;
-}
-.mu-plusbox {
-    background-color: #ffffff !important;
-    border: 1px dashed #818181 !important;
-    border-radius: 5px !important;
-    cursor: pointer !important;
-    display: flex !important;
-    flex-wrap: wrap !important;
-    align-items: center !important;
-    width: 140px !important;
-    height: 90px !important;
-}
-.mu-plusbox:hover{
-    background-color: #f1f1f1 !important;
-}
-.mu-plusbox:hover > .mu-plus-icon{
-    color: #028296 !important;
-}
-.mu-plus-icon{
-    color: #00afca !important;
-    font-size: 3rem !important;
-    flex: 1;
-}
+    .mu-plusbox-container{
+        display: inline-flex !important;
+        height: 90px !important;
+        width: 140px !important;
+        margin: 0.25rem !important;
+    }
+    .mu-plusbox {
+        background-color: #ffffff !important;
+        border: 1px dashed #818181 !important;
+        border-radius: 5px !important;
+        cursor: pointer !important;
+        display: flex !important;
+        flex-wrap: wrap !important;
+        align-items: center !important;
+        width: 140px !important;
+        height: 90px !important;
+    }
+    .mu-plusbox:hover{
+        background-color: #f1f1f1 !important;
+    }
+    .mu-plusbox:hover > .mu-plus-icon{
+        color: #028296 !important;
+    }
+    .mu-plus-icon{
+        color: #00afca !important;
+        font-size: 3rem !important;
+        flex: 1;
+    }
 
-/* ----media-preview---- */
+    /* ----media-preview---- */
 
-.mu-image-container{
-    width: 140px !important;
-    height: 90px !important;
-    margin: 0.25rem !important;
-    
-    position: relative;
-}
-.mu-images-preview {
-    border-radius: 5px !important;
-    border: 1px solid #818181 !important;
-    width: 140px !important;
-    height: 90px !important;
-    transition: filter 0.1s linear;
-    object-fit: cover;
-    object-position: center;
-}
-.mu-images-preview:hover{
-    filter: brightness(90%);
-}
+    .mu-image-container{
+        width: 140px !important;
+        height: 90px !important;
+        margin: 0.25rem !important;
 
-.mu-close-btn{
-    background: none !important;
-	color:white !important;
-	border: none !important;
-	padding: 0px !important;
-    margin:0px !important;
-	cursor: pointer !important;
+        position: relative;
+    }
+    .mu-images-preview,
+    .mu-images-preview2 {
+        border-radius: 5px !important;
+        border: 1px solid #818181 !important;
+        width: 140px !important;
+        height: 90px !important;
+        transition: filter 0.1s linear;
+        object-fit: cover;
+        object-position: center;
+    }
+    .mu-images-preview:hover{
+        filter: brightness(90%);
+    }
 
-    position: absolute !important;
-    top: 0px;
-    right: 0px;
-}
-.mu-times-icon{
-    font-size: 3rem !important;
-    filter: drop-shadow(0px 0px 1px black);
-}
-.mu-close-btn:hover{
-    color: red !important;
-}
+    button:disabled {
+        opacity: 0.4;
+    }
 
-/* -------------------- */
+    .mu-close-btn{
+        display:block;
+        background: none !important;
+        color:white !important;
+        border: none !important;
+        padding: 0px !important;
+        margin:0px !important;
+        cursor: pointer !important;
+        position: absolute !important;
+        top: 0px;
+        right: 3px;
+    }
 
-.mu-red-border {
-    border: 1px solid #dc3545 !important;
-}
+    .mu-left-rotate-btn {
+        display:block;
+        background: none !important;
+        color:white !important;
+        border: none !important;
+        padding: 0px !important;
+        margin:0px !important;
+        cursor: pointer !important;
+        position: absolute !important;
+        top: 20px;
+        right: 3px;
+    }
+    .mu-right-rotate-btn {
+        display:block;
+        background: none !important;
+        color:white !important;
+        border: none !important;
+        padding: 0px !important;
+        margin:0px !important;
+        cursor: pointer !important;
+        position: absolute !important;
+        top: 40px;
+        right: 3px;
+    }
+    .mu-times-icon{
+        font-size: 3rem !important;
+        filter: drop-shadow(0px 0px 1px black);
+    }
+    .mu-close-btn:hover{
+        color: red !important;
+    }
+    .mu-left-rotate-btn:hover,
+    .mu-right-rotate-btn:hover {
+        color: yellow !important;
+    }
 
-.mu-mt-1 {
-  margin-top: 0.25rem !important;
-}
+    /* -------------------- */
 
-/* -------------------- */
+    .mu-red-border {
+        border: 1px solid #dc3545 !important;
+    }
 
-img {
-  -webkit-user-drag: none;
-  -khtml-user-drag: none;
-  -moz-user-drag: none;
-  -o-user-drag: none;
-}
+    .mu-mt-1 {
+        margin-top: 0.25rem !important;
+    }
+
+    /* -------------------- */
+
+    img {
+        -webkit-user-drag: none;
+        -khtml-user-drag: none;
+        -moz-user-drag: none;
+        -o-user-drag: none;
+    }
 
 </style>
